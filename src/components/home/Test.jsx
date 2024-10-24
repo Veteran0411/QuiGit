ad65d714c1d4107be765938d90573ff2c9734c1c
Veteran0411 <somlinghajji@gmail.com>
2024-10-24 12:56:37 +0530

    modified test.jsx

--- src/components/home/Test.jsx
+++ src/components/home/Test.jsx
@@ -5,13 +5,15 @@ const Test = () => {
     const isAuthenticated=useSelector((state)=>state.auth.isAuthenticated)
   return (
       <div>
-          <h1>test</h1>
-        {
-        isAuthenticated ? (
-        <h6>Welcome Back!</h6>
-      ) : (
-        <h6>Please Log In</h6>
-      )}
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+          
+        </div>
       </div>
   )
 }
