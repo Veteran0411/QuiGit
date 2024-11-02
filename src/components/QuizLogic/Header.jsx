3783cb2feb299b0de1a8e54fce62e5731fc048b8
Rohit Narayan Bhat <rohitbhat7032@gmail.com>
2024-11-02 11:42:06 +0530

    pulled from main branch

---
+++ src/components/QuizLogic/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="app-header">
+      {/* <img src="quigit.jpg" alt="logo" /> */}
+      <h1> QuiGit</h1>
+    </header>
+  );
+}
+
+export default Header;
